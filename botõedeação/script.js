7ef7844d5f9924f68787e68f1b2492e7bc372373
Jacó da ak47 <54730694+Jacobyto@users.noreply.github.com>
2023-06-15 12:15:11 -0300

    Add files via upload

---
+++ botõedeação/script.js
@@ -0,0 +1,24 @@
+let resp = window.document.getElementById('saída')
+
+function acao1() {
+resp.innerHTML += '<p>Biiiiiiiiiirl</p>'
+}
+function acao2() {
+resp.innerHTML += '<p>aoba</p>'
+
+}
+
+function acao3(){
+resp.innerHTML += '<p>Truco marréco</p>'
+
+}
+
+function acao4(){
+    resp.innerHTML += '<p>sai rapariga</p>'
+
+}
+
+function acao5(){
+    resp.innerHTML += '<p>Olha, se você não me ama, então não me ligue, não fique me fazendo queixas</p>'
+
+}
